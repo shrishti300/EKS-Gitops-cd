65d2fb0ba735124086c408fbfecfd27dd8bfbf3d
shrishti gupta <shrishtigupta@shrishtis-MacBook-Pro.local>
2024-06-03 11:33:05 +0530

    gitops assignmet

--- EKS-Application-ci/second_spring_boot_to_RDS/src/main/java/com/example/demo/repository/ExchangeRateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.ExchangeRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Integer> {
-}
